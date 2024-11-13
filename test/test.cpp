3ff745ee0b7180849abe069ee9eb87d6a62ed8da
Famiu Haque <famiuhaque@proton.me>
2024-11-13 06:14:01 +0600

    test: add tests for multiplication and division

--- test/test.cpp
+++ test/test.cpp
@@ -2,6 +2,9 @@
 
 #include "bigint.hpp"
 
+// Disable linting for Catch2 macros.
+// NOLINTBEGIN(cppcoreguidelines-avoid-do-while,bugprone-chained-comparison)
+
 TEST_CASE("BigInt default constructor")
 {
     BigInt c;
@@ -23,12 +26,15 @@ TEST_CASE("BigInt integral constructor")
     {
         REQUIRE(BigInt(1234567890) == 1234567890);
         REQUIRE(BigInt(987654321) == 987654321);
+        REQUIRE(BigInt(9223372036854775807LL) == 9223372036854775807LL);
+        REQUIRE(BigInt(18446744073709551615ULL) == 18446744073709551615ULL);
     }
 
     SECTION("Negative numbers")
     {
         REQUIRE(BigInt(-1234567890) == -1234567890);
         REQUIRE(BigInt(-987654321) == -987654321);
+        REQUIRE(BigInt(-9223372036854775807LL) == -9223372036854775807LL);
     }
 }
 
@@ -44,12 +50,15 @@ TEST_CASE("BigInt String constructor")
     {
         REQUIRE(BigInt("1234567890") == 1234567890);
         REQUIRE(BigInt("987654321") == 987654321);
+        REQUIRE(BigInt("9223372036854775807") == 9223372036854775807LL);
+        REQUIRE(BigInt("18446744073709551615") == 18446744073709551615ULL);
     }
 
     SECTION("Negative numbers")
     {
         REQUIRE(BigInt("-1234567890") == -1234567890);
         REQUIRE(BigInt("-987654321") == -987654321);
+        REQUIRE(BigInt("-9223372036854775807") == -9223372036854775807LL);
     }
 
     SECTION("Prefixed numbers")
@@ -57,6 +66,8 @@ TEST_CASE("BigInt String constructor")
         REQUIRE(BigInt("0x1234567890") == 0x1234567890);
         REQUIRE(BigInt("01234567") == 01234567);
         REQUIRE(BigInt("0b1010101") == 0b1010101);
+        REQUIRE(BigInt("0x7fffffffffffffff") == 0x7fffffffffffffffLL);
+        REQUIRE(BigInt("0xffffffffffffffff") == 0xffffffffffffffffULL);
     }
 
     SECTION("Negative prefixed numbers")
@@ -64,6 +75,7 @@ TEST_CASE("BigInt String constructor")
         REQUIRE(BigInt("-0x1234567890") == -0x1234567890);
         REQUIRE(BigInt("-01234567") == -01234567);
         REQUIRE(BigInt("-0b1010101") == -0b1010101);
+        REQUIRE(BigInt("-0x7fffffffffffffff") == -0x7fffffffffffffffLL);
     }
 }
 
@@ -79,12 +91,15 @@ TEST_CASE("BigInt Literals")
     {
         REQUIRE(1234567890_bi == 1234567890);
         REQUIRE(987654321_bi == 987654321);
+        REQUIRE(9223372036854775807_bi == 9223372036854775807LL);
+        REQUIRE(18446744073709551615_bi == 18446744073709551615ULL);
     }
 
     SECTION("Negative decimal numbers")
     {
         REQUIRE(-1234567890_bi == -1234567890);
         REQUIRE(-987654321_bi == -987654321);
+        REQUIRE(-9223372036854775807_bi == -9223372036854775807LL);
     }
 
     SECTION("Prefixed numbers")
@@ -92,6 +107,8 @@ TEST_CASE("BigInt Literals")
         REQUIRE(0x1234567890_bi == 0x1234567890);
         REQUIRE(01234567_bi == 01234567);
         REQUIRE(0b1010101_bi == 0b1010101);
+        REQUIRE(0x7fffffffffffffff_bi == 0x7fffffffffffffffLL);
+        REQUIRE(0xffffffffffffffff_bi == 0xffffffffffffffffULL);
     }
 
     SECTION("Negative prefixed numbers")
@@ -99,6 +116,7 @@ TEST_CASE("BigInt Literals")
         REQUIRE(-0x1234567890_bi == -0x1234567890);
         REQUIRE(-01234567_bi == -01234567);
         REQUIRE(-0b1010101_bi == -0b1010101);
+        REQUIRE(-0x7fffffffffffffff_bi == -0x7fffffffffffffffLL);
     }
 }
 
@@ -143,3 +161,43 @@ TEST_CASE("BigInt Subtraction")
     REQUIRE((-a) - b == -2222222211_bi);
     REQUIRE((-a) - (-b) == -246913569_bi);
 }
+
+TEST_CASE("BigInt Bitshift")
+{
+    REQUIRE(a << 1 == 2469135780_bi);
+    REQUIRE(a >> 1 == 617283945_bi);
+    REQUIRE(b << 1 == 1975308642_bi);
+    REQUIRE(b >> 1 == 493827160_bi);
+    REQUIRE(a << 10 == 1264197519360_bi);
+    REQUIRE(a >> 10 == 1205632_bi);
+    REQUIRE(a >> 100 == 0_bi);
+    REQUIRE(b << 100 == 1252000592833654354567462788044156829696_bi);
+}
+
+TEST_CASE("BigInt Multiplication")
+{
+    REQUIRE(a * b == 1219326311126352690_bi);
+    REQUIRE(a * (-b) == -1219326311126352690_bi);
+    REQUIRE((-a) * b == -1219326311126352690_bi);
+    REQUIRE((-a) * (-b) == 1219326311126352690_bi);
+}
+
+TEST_CASE("BigInt Division and Modulo")
+{
+    REQUIRE(a / b == 1_bi);
+    REQUIRE(a / (-b) == -1_bi);
+    REQUIRE((-a) / b == -1_bi);
+    REQUIRE((-a) / (-b) == 1_bi);
+    REQUIRE(a % b == 246913569_bi);
+    REQUIRE(a % (-b) == 246913569_bi);
+    REQUIRE((-a) % b == -246913569_bi);
+    REQUIRE((-a) % (-b) == -246913569_bi);
+
+    BigInt c = 106048574244834508800_bi;
+    BigInt d = -429391241160_bi;
+
+    REQUIRE(c / d == -246974237_bi);
+    REQUIRE(c % d == 84860513880_bi);
+}
+
+// NOLINTEND(cppcoreguidelines-avoid-do-while,bugprone-chained-comparison)
